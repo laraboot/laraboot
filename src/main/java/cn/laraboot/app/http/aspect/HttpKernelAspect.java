e1de6ee005e9612d90f3b65a867866b5cf809f0f
qbhy <96qbhy@gmail.com>
2019-09-28 14:20:42 +0800

    完善基础框架

---
+++ src/main/java/cn/laraboot/app/http/aspect/HttpKernelAspect.java
@@ -0,0 +1,22 @@
+package cn.laraboot.app.http.aspect;
+
+import cn.laraboot.framework.http.aspect.HttpAspect;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class HttpKernelAspect extends HttpAspect {
+
+    @Pointcut("execution(public * cn.laraboot.app.http.controllers..*(..))")
+    public void controllerAspect() {
+    }
+
+    @Around("controllerAspect()")
+    public Object handle(ProceedingJoinPoint point) throws Throwable {
+        return this.aroundPointcut(point);
+    }
+}
