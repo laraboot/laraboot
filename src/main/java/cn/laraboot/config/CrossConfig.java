2790c7aa2854b933224653363ad78925fc68d4a0
qbhy <96qbhy@gmail.com>
2019-10-08 15:00:42 +0800

    添加spring-boot-configuration-processor依赖

--- src/main/java/cn/laraboot/config/CrossConfig.java
+++ src/main/java/cn/laraboot/config/CrossConfig.java
@@ -1,6 +1,7 @@
 package cn.laraboot.config;
 
 import cn.laraboot.framework.http.middlewares.cross.Cross;
+import cn.laraboot.framework.http.middlewares.cross.CrossMiddleware;
 import cn.laraboot.framework.http.middlewares.cross.CrossProvider;
 import cn.laraboot.framework.http.middlewares.cross.Crosses;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,12 @@ import org.springframework.http.HttpMethod;
 
 @Configuration
 public class CrossConfig {
+
+    @Bean
+    public CrossMiddleware crossMiddleware(CrossProvider provider){
+        return new CrossMiddleware(provider);
+    }
+
     @Bean
     public CrossProvider crossProvider() {
         return () -> new Crosses()
